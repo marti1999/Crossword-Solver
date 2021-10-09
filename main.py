c571a0de9157f5edaa2d1ad404687a0a92e476bd
SergiBons <sbonsf@gmail.com>
2021-10-10 00:05:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,61 +2,185 @@ import copy
 import math
 import numpy as np
 import sys
+import random
 import time
 # https://plugins.jetbrains.com/plugin/16536-line-profiler
 from line_profiler_pycharm import profile
 
-#pasa el taulell a INT
-def taulellStringToINT(taulell):
-    result = []
-    for j in range(0, (len(taulell))):
-        linea = taulell[j].split()
+class Intersection:
+    def __init__(self, coord, index, intersectedID):
+        self.coord = coord
+        self.index = index
+        self.intersectedID = intersectedID
 
-        if "#" in linea:
-            for index, element in enumerate(linea):
-                if element == '#':
-                    linea[index] = "-1"
+class Word:
+    def __init__(self, pos, horizontal, length, remainingValues, idN):
+        self.pos = pos
+        self.horizontal = horizontal
+        self.length = length
+        self.remainingValues = remainingValues
+        self.letters = [0] * length
+        self.id = idN
+        self.intersections = []
 
-        linea = np.array(linea, dtype=(int))
-        result.append(linea)
-    return result
+# function to know this word is vertical
+# return the id of word vertical or -1 if not is vertical
+def getIdByCoordVertical(coord, verticalWords):
+    for w in verticalWords:
+        if w.pos[1] == coord[1]:
+            if w.pos[0] <= coord[0] <= w.pos[0]+w.length:
+                return w.id
+    return -1
 
-#pasa el taulell a String
-def taulellINTtoString(taulell):
-    linea = []
-    result = []
-    for j in range(0, (len(taulell))):
-        linea = taulell[j]
-        linea[linea == -1] = 35
-        linea = linea.tostring().decode("ascii")
-        linea = list(linea)
-        result.append(linea)
-    return result
+# function to know this word is horizontal
+# return the id of word horizontal or -1 if not is horizontal
+def getIdByCoordHorizontal(coord, horizontalWords):
+    for w in horizontalWords:
+        if w.pos[0] == coord[0]:
+            if w.pos[1] <= coord[1] <= w.pos[1]+w.length:
+                return w.id
+    return -1
 
-# Obtenim el taulell
-def obtenirTaulell(crosswordFile):
-    taulell = []
-    for linea in open(crosswordFile):
-        taulell.append(linea)
-    return taulellStringToINT(taulell)
 
-#Impresió del taulell
-def imprimirTaulell(taulell):
-    for i in range(0,len(taulell)):
-        for j in range(0,len(taulell[i])):
-            sys.stdout.write(taulell[i][j])
+def lookupIntersections(words, horizontalWords, verticalWords, crossword):
+    for w in words:
+        if w.horizontal == 1:
+            yStart = w.pos[1]
+            yEnd = yStart + w.length -1
+            for y in range(yStart, yEnd+1):
+                if w.pos[0] > 0:
+                    if crossword[w.pos[0]-1][y] == 0:
+                        index = y - w.pos[1]
+                        intersectedId = getIdByCoordVertical((w.pos[0], y), verticalWords)
+                        w.intersections.append(Intersection((w.pos[0], y), index, intersectedId))
+                        continue
+                if w.pos[0] < crossword.shape[0]-1:
+                    if crossword[w.pos[0]+1][y] == 0:
+                        index = y - w.pos[1]
+                        intersectedId = getIdByCoordVertical((w.pos[0],y), verticalWords)
+                        w.intersections.append(Intersection((w.pos[0], y), index, intersectedId))
+        else:
+            xStart = w.pos[0]
+            xEnd = xStart + w.length -1
+            for x in range(xStart, xEnd+1):
+                if w.pos[1] > 0:
+                    if crossword[x][w.pos[1]-1] == 0:
+                        index = x - w.pos[0]
+                        intersectedId = getIdByCoordHorizontal((x, w.pos[1]), horizontalWords)
+                        w.intersections.append(Intersection((x, w.pos[1]), index, intersectedId))
+                        continue
+                if w.pos[1] < crossword.shape[1]-1:
+                    if crossword[x][w.pos[1]+1] == 0:
+                        index =  x - w.pos[0]
+                        intersectedId = getIdByCoordHorizontal((x, w.pos[1]), horizontalWords)
+                        w.intersections.append(Intersection((x, w.pos[1]), index, intersectedId))
 
-# arxius dels que s'agafen les dades
+    return words
+
+
+def read_crossword(crossword):
+    table = []
+    for line in open(crossword):
+        noTabs = list(line.split())
+        for i, v in enumerate(noTabs):
+            if v == "#":
+                noTabs[i] = "35"
+        table.append(noTabs)
+    npTable = np.array(table, dtype=np.uint8)
+    return npTable
+
+
+def storeLvaToCrossword(lva, crossword):
+    for word in lva.values():
+        index = 0
+        if word.horizontal == 1:
+            x = word.pos[0]
+            for y in range(word.pos[1], word.pos[1] + word.length):
+                crossword[x][y] = word.letters[index]
+                index += 1
+        else:
+            y = word.pos[1]
+            for x in range(word.pos[0], word.pos[0] + word.length):
+                crossword[x][y] = word.letters[index]
+                index += 1
+    return crossword
+
+
+def printCrossword(crossword):
+    # crossword = np.where(crossword[:] ==1, '#', crossword)
+
+    #TODO descomentar i deixar mostrant les lletres i no els números
+
+    print('\n'.join([''.join(['{:4}'.format(chr(item))
+                              for item in row]) for row in crossword]))
+    # print('\n'.join([''.join(['{:4}'.format(item)
+    #                           for item in row]) for row in crossword]))
+    print("\n\n\n")
+
+
+def lookupHorizontalVariables(npTable, idN):
+    foundWords = []
+
+    for x in range(0, npTable.shape[0]):
+        len = 0
+        for y in range(1, npTable.shape[1]):
+            if len == 0:
+                if npTable[x][y - 1] != 35 and npTable[x][y] != 35:
+                    len = 2
+                    continue
+            if len > 1:
+                if npTable[x][y] != 35:
+                    len += 1
+                    continue
+                else:
+                    pos = (x, y - len)
+                    foundWords.append(Word(pos, 1, len, len, idN))
+                    idN += 1
+                    len = 0
+
+        if len > 1:
+            pos = (x, npTable.shape[1] - len)
+            word = Word(pos, 1, len, len, idN)
+            idN += 1
+            foundWords.append(word)
+    return foundWords
+
+
+def lookupVerticalVariables(npTable, idN):
+    foundWords = []
+    for y in range(0, npTable.shape[1]):
+        len = 0
+        for x in range(1, npTable.shape[0]):
+            if len == 0:
+                if npTable[x - 1][y] != 35 and npTable[x][y] != 35:
+                    len = 2
+                    continue
+
+            if len > 1:
+                if npTable[x][y] != 35:
+                    len += 1
+                    continue
+                else:
+                    pos = (x - len, y)
+                    foundWords.append(Word(pos, 0, len, len, idN))
+                    idN += 1
+                    len = 0
+        if len > 1:
+            pos = (npTable.shape[0] - len, y)
+            word = Word(pos, 0, len, len, idN)
+            idN += 1
+            foundWords.append(word)
+    return foundWords
+
+
 def seleccioTest():
-
     crossword = "crossword_CB_v2.txt"
-    diccionari = "diccionari_A.txt"
+    diccionari = "diccionari_CB_v2.txt"
 
-    return obtenirTaulell(crossword), diccionari
+    return crossword, diccionari
 
-@profile
+
 def classificarDiccionari(dictPath):
-
     # Dictionary with all the words.
     # Each key is the size of the word.
     # Each value is a 2D numpy array. The first dimension is the word,
@@ -82,20 +206,86 @@ def classificarDiccionari(dictPath):
 
     return dict
 
+def domain(var, d):
+    return d[var.length]
 
-if __name__ == '__main__':
-    taulell, dicpath = seleccioTest()
+def restrictionsOK(var, cWord, lva, r):
 
+    intersections = var.intersections
+
+    for i in intersections:
+        if i.intersectedID not in lva:
+            continue
+
+        intersectedWord = lva[i.intersectedID]
+        candidateValue = cWord[i.index]
+
+        if intersectedWord.horizontal == 1:
+            intersectionIndex = i.coord[1] - intersectedWord.pos[1]
+            intersectedValue = intersectedWord.letters[intersectionIndex]
+        else:
+            intersectionIndex = i.coord[0] - intersectedWord.pos[0]
+            intersectedValue = intersectedWord.letters[intersectionIndex]
+
+        if intersectedValue != candidateValue:
+            return False
+
+    return True
+
+def insertLva(lva, var, cWord):
+    var.letters = cWord.tolist()
+    lva[var.id] = var
+    return lva
+
+def backtracking(lva, lvna, d, r):
+
+    if not lvna:
+        return lva, 1
+
+    var = lvna[0]
+
+    domainValues = domain(var, d)
+    for cWord in domainValues:
+
+        if restrictionsOK(var, cWord, lva, 0):
+            lva = insertLva(lva, var, cWord)
+            lva, r = backtracking(lva, lvna[1:], d, r)
+            if r == 1:
+                return lva, r
+
+    # if not is complete the cossword and word is in LVA
+    # delete from LVA because the word no use for the solution
+    if r == 0 and var.id in lva:
+        lva.pop(var.id)
+
+    return lva, 0
+
+def main():
+    crosswordPath, dicPath = seleccioTest()
+
+    crossword = read_crossword(crosswordPath)
 
     start = time.time()
+    horizontalWords = lookupHorizontalVariables(crossword, 0)
+    verticalWords = lookupVerticalVariables(crossword, len(horizontalWords))
 
-    dic = classificarDiccionari(dicpath)
+    words = horizontalWords + verticalWords
+    words.sort(key=lambda x: x.remainingValues)
+    #random.shuffle(words)
 
+    words = lookupIntersections(words, horizontalWords, verticalWords, crossword)
+
+    dict = classificarDiccionari(dicPath)
+
+    lva, r = backtracking({}, words, dict, 0)
+
+    crossword = storeLvaToCrossword(lva, crossword)
+    printCrossword(crossword)
 
     end = time.time()
-    temps = end-start
+    temps = end - start
     print("temps: %d", temps)
 
 
-
-
+if __name__ == '__main__':
+    main()
